ff6a2c4258e52df67a2be70d1dedf771d6170b64
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-04-07 20:01:56 +0530

    JSON response with hit number recieving

--- app/controllers/lead_gen_controller.rb
+++ app/controllers/lead_gen_controller.rb
@@ -1,4 +1,11 @@
+require 'elasticsearch'
+
 class LeadGenController < ApplicationController
 	def choose_name
+	end
+	def take_decision
+		client = Elasticsearch::Client.new log: true
+		json_data = client.search index: 'company', body: { query: { match_phrase: { company_name: params["company_name"] } } }
+		render :json => json_data
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 'choose_name' => 'lead_gen#choose_name'
+  post 'lead_gen/take_decision' => 'lead_gen#take_decision'
 end

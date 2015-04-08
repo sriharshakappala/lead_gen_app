a60818d89495e2c16704628793800666fb13ca9d
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-04-08 11:17:18 +0530

    Decision enabled via Service

--- app/controllers/lead_gen_controller.rb
+++ app/controllers/lead_gen_controller.rb
@@ -4,8 +4,8 @@ class LeadGenController < ApplicationController
 	def choose_name
 	end
 	def take_decision
-		client = Elasticsearch::Client.new log: true
-		json_data = client.search index: 'company', body: { query: { match_phrase: { company_name: params["company_name"] } } }
+		name_approver = NameApprover.new(params["company_name"])
+		json_data = name_approver.take_decision
 		render :json => json_data
 	end
 end
